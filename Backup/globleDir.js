65a9d49e09d26c3cde95bbb1d7361e6fdeb4f4fb
prashanksingh93-source <prashanksingh93@gmail.com>
2026-07-05 18:51:27 +0530

    Create globleDir.js

---
+++ Backup/globleDir.js
@@ -0,0 +1,10 @@
+console.log(__dirname);
+console.log(__filename);
+
+const fs=require('fs')
+
+console.log(process.cwd);
+console.log(process.pid);
+console.log(process.platform);
+console.log(process.env.USER);
+
