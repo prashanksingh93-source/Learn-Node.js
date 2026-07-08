02a3236fd896cda6c6e829d627841753d4a10e4f
prashanksingh93-source <prashanksingh93@gmail.com>
2026-07-08 18:16:28 +0530

    Update aliased-import.js

--- Backup/aliased-import/aliased-import.js
+++ Backup/aliased-import/aliased-import.js
@@ -1,3 +1,5 @@
+// An aliased import means importing a module using a different local name than its original export. This is useful to avoid naming conflicts or to use a shorter or clearer name.
+
 import * as math from './math.js'
 
 console.log(math.add(2,3));
